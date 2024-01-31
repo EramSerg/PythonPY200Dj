60ff00eadfe77cf3ebaa154e54c636efbc5b240a
Eram Sergey <139155326+EramSerg@users.noreply.github.com>
2024-01-31 11:15:55 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,7 +3,22 @@ from .models import get_random_text
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
-from .forms import TemplateForm
+from .forms import TemplateForm, CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+"""def template_view(request):
+    if request.method == "GET":
+        return render(request, 'app/template_form.html')
+
+    if request.method == "POST":
+        received_data = request.POST  # Приняли данные в словарь
+        # как пример получение данных по ключу `my_text`
+        # my_text = received_data.get('my_text')
+        data = {'user_name': received_data.get('user_name'), 'user_email': received_data.get('user_email'),
+                'user_password': received_data.get('user_password'), 'input_int': received_data('input_int')}
+
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})"""
 
 
 def template_view(request):
@@ -13,12 +28,19 @@ def template_view(request):
     if request.method == "POST":
         received_data = request.POST  # Приняли данные в словарь
 
-        # как пример получение данных по ключу `my_text`
-        # my_text = received_data.get('my_text')
+        form = TemplateForm(received_data)  # Передали данные в форму
+        if form.is_valid():  # Проверили, что данные все валидные
+            all_data = {'my_text': form.cleaned_data.get("my_text"), 'my_select': form.cleaned_data.get("my_select"),
+                        'my_textarea': form.cleaned_data.get("my_textarea"),
+                        'user_name': form.cleaned_data.get("user_name"),
+                        'user_email': form.cleaned_data.get("user_email"),
+                        'user_password': form.cleaned_data.get("user_password"),
+                        'input_date': form.cleaned_data.get("input_date"),
+                        'input_int': form.cleaned_data.get("input_int"), 'checkbox': form.cleaned_data.get("checkbox")}
 
-        # TODO Проведите здесь получение и обработку данных если это необходимо
+            return JsonResponse(all_data, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
 
-        # TODO Верните HttpRequest или JsonResponse с данными
+        return render(request, 'app/template_form.html', context={"form": form})
 
 
 def login_view(request):
@@ -26,12 +48,20 @@ def login_view(request):
         return render(request, 'app/login.html')
 
     if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')  # Авторизируем пользователя
+            return redirect("app:user_profile")
+        return render(request, "app/login.html", context={"form": form})
+
+    '''if request.method == "POST":
         data = request.POST
         user = authenticate(username=data["username"], password=data["password"])
         if user:
             login(request, user)
             return redirect("app:user_profile")
-        return render(request, "app/login.html", context={"error": "Неверные данные"})
+        return render(request, "app/login.html", context={"error": "Неверные данные"})'''
 
 
 def logout_view(request):
@@ -45,7 +75,16 @@ def register_view(request):
         return render(request, 'app/register.html')
 
     if request.method == "POST":
-        return render(request, 'app/register.html')
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Возвращает сохраненного пользователя из данных формы
+            login(request, user)
+            return redirect("app:user_profile")
+
+        return render(request, 'app/register.html', context={"form": form})
+
+    '''if request.method == "POST":
+        return render(request, 'app/register.html')'''
 
 
 def reset_view(request):
@@ -66,6 +105,7 @@ def index_view(request):
 def user_detail_view(request):
     if request.method == "GET":
         return render(request, 'app/user_details.html')
+
 
 def get_text_json(request):
     if request.method == "GET":
